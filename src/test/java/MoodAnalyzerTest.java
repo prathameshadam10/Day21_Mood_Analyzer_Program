53e09a33a2c24b9d63f8ef15ab004444cc6e1ffe
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-04-03 20:11:23 +0530

    Update MoodAnalyzerTest.java

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,2 +1,26 @@
+import com.bridgelabz.exceptions.MoodAnalyzer;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
+    @Test
+    public void analyzeSadMessageTest(){
+        MoodAnalyzer moodAnalyzer  = new MoodAnalyzer();
+        Assert.assertEquals("SAD", moodAnalyzer.analyzeMood("I am in Sad Mood"));
+    }
+    @Test
+    public void analyzeHappyMessageTest(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        Assert.assertEquals("HAPPY", moodAnalyzer.analyzeMood("I am in Any Mood"));
+    }
+    @Test
+    public void analyzeSadMessageTestWithConstructor(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        Assert.assertEquals("SAD", moodAnalyzer.analyzeMoodWithConstructor());
+    }
+    @Test
+    public void analyzeHappyMessageTestWithConstructor(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Any Mood");
+        Assert.assertEquals("Happy", moodAnalyzer.analyzeMoodWithConstructor());
+    }
 }
